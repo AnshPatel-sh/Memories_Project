309b7c5f61193581baf13d6287655a806cbb32ba
AnshPatel-sh <patanshb@sheridancollege.ca>
2024-07-31 21:52:06 -0400

    last stage changes

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ import cors from "cors";
 import bodyParser from "body-parser";
 
 import postRoutes from "./routes/posts.js";
+import dotenv from 'dotenv'
 
+dotenv.config(); 
 const app = express();
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -13,14 +16,17 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://ansh:ansh123@cluster0.oasoiyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     app.listen(PORT, () => {
+      console.log(`Database connection is successful:${mongoose.connection.host}`)
       return console.log(`Server is running on port:${PORT}`);
     });
   })
